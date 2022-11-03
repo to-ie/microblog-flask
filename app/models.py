ce1b4c2a94db8ca115f6a53d66e510b3aa0fe008
theodorekall <theodorekall@protonmail.com>
2022-11-03 18:47:05 +0000

    avatars and edit profile

--- app/models.py
+++ app/models.py
@@ -13,6 +13,7 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
